909d993ebdb9efc552ce10ff7c949d846c37a8cc
Axl <74795782+Zellon19@users.noreply.github.com>
2024-09-08 23:33:21 -0300

    bah

---
+++ main.js
@@ -0,0 +1,33 @@
+console.log('Hello, World!');
+
+require('dotenv').config();
+const { app, BrowserWindow, ipcMain } = require('electron/main');
+const path = require('node:path');
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            preload: path.join(__dirname, 'preload.js')
+        },
+        title: 'My First Electron App',
+    });
+    mainWindow.setTitle('My App');
+    mainWindow.loadFile('./index.html');
+};
+
+app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong');
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
